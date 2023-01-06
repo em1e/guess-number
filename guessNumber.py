a6a0940f136d858f275e12b501ad971474e14d15
Emi <113087578+em1e@users.noreply.github.com>
2023-01-06 20:50:07 +0200

    Update guessNumber.py

--- guessNumber.py
+++ guessNumber.py
@@ -1,13 +1,15 @@
-    import random
+import random
 answer = random.randint(1, 10)
-print(answer)
+#print(answer)
 while True:
     try:
         guess = int(input('Guess a number between 1 and 10: '))
         if 0 < int(guess) < 11:
             if int(guess) == answer:
-                print('you are a genius')
+                print('You are a genius!')
                 break
+            else:
+              print('Wrong answer, try again')
     except ValueError:
-        print('please enter a number')
+        print('Please enter a number')
         continue
